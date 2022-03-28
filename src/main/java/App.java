8d4f664eb69c2e259ffa2c76d456c9b2eda8c31d
Node-23 <matheus.dev@hotmail.com>
2022-03-28 19:57:03 -0300

    Create Password View created

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,10 @@
+import View.CreatePasswordView;
 import View.HomeView;
 import View.LoginView;
 import View.RegisterView;
 
 public class App {
     public static void main(String[] args) {
-        //LoginView.ShowLoginView();
-        HomeView.ShowHomeView();
+        LoginView.ShowLoginView();
     }
 }
